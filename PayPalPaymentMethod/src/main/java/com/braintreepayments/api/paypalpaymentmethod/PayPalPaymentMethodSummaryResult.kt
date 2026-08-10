a567692af9cef4e52539beb14dcb6cb543d99b59
Mageshwara <37831118+Mageshwara@users.noreply.github.com>
2026-08-10 09:23:08 +0530

    refactor(PayPalPaymentMethodClient): rename SavedPayPalPaymentMethodClient to PayPalPaymentMethodClient

--- PayPalPaymentMethod/src/main/java/com/braintreepayments/api/paypalpaymentmethod/PayPalPaymentMethodSummaryResult.kt
+++ PayPalPaymentMethod/src/main/java/com/braintreepayments/api/paypalpaymentmethod/PayPalPaymentMethodSummaryResult.kt
@@ -3,7 +3,7 @@ package com.braintreepayments.api.paypalpaymentmethod
 import com.braintreepayments.api.core.ExperimentalBetaApi
 
 /**
- * Result of [SavedPayPalPaymentMethodClient.fetchFI] / [SavedPayPalPaymentMethodClient.refetchFI].
+ * Result of [PayPalPaymentMethodClient.fetchFI] / [PayPalPaymentMethodClient.refetchFI].
  */
 @ExperimentalBetaApi
 sealed class PayPalPaymentMethodSummaryResult {
